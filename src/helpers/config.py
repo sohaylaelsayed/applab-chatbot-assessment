92c8e59e60ce88eabf898f801d9f09eae83be75a
sohayla <sohayla@cloudai.solutions>
2024-12-21 18:41:16 +0300

    dockerize both chatbot service and chatbot ui

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -2,27 +2,27 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
 
-    APP_NAME: str
-    APP_VERSION: str
+    APP_NAME: str = "applab-assessment"
+    APP_VERSION: str = "0.1.0"
 
-    FILE_ALLOWED_TYPES: list
-    FILE_MAX_SIZE: int
-    FILE_DEFAULT_CHUNK_SIZE: int 
+    FILE_ALLOWED_TYPES: list = ["application/pdf"]
+    FILE_MAX_SIZE: int = 10
+    FILE_DEFAULT_CHUNK_SIZE: int = 512000 # 512KB  
 
     EMBEDDING_BACKEND: str = "OPENAI"
 
     OPENAI_API_KEY: str = None
 
-    EMBEDDING_MODEL_ID: str = None
-    EMBEDDING_MODEL_SIZE: int = None
-    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    EMBEDDING_MODEL_ID: str = "text-embedding-3-small"
+    EMBEDDING_MODEL_SIZE: int = 1536
+    INPUT_DAFAULT_MAX_CHARACTERS: int = 8191
     GENERATION_DAFAULT_MAX_TOKENS: int = 200
     GENERATION_DAFAULT_TEMPERATURE: float = 0.1 
 
     
-    VECTOR_DB_BACKEND : str
-    VECTOR_DB_PATH : str
-    VECTOR_DB_DISTANCE_METHOD: str = None
+    VECTOR_DB_BACKEND : str = "QDRANT"
+    VECTOR_DB_PATH : str = "VECTOR_DB_PATH"
+    VECTOR_DB_DISTANCE_METHOD: str = "cosine"
 
 
 
